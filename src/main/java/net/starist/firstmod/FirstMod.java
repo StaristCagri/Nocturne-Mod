6f5603245d1806d7b2bfa854c818f65d77ec4a15
StaristCagri <cagriyasin2005@gmail.com>
2025-03-09 15:54:57 +0300

    Update 0.0.2

--- src/main/java/net/starist/firstmod/FirstMod.java
+++ src/main/java/net/starist/firstmod/FirstMod.java
@@ -1,6 +1,7 @@
 package net.starist.firstmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,6 +14,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.starist.firstmod.item.Moditems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -30,7 +32,7 @@ public class FirstMod
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-
+        Moditems.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -47,6 +49,9 @@ public class FirstMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if(event.getTabKey()== CreativeModeTabs.INGREDIENTS){
+            event.accept(Moditems.NOCTIUM);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/starist/firstmod/item/Moditems.java
@@ -0,0 +1,20 @@
+package net.starist.firstmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.starist.firstmod.FirstMod;
+
+public class Moditems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FirstMod.MOD_ID);
+
+    public static final RegistryObject<Item> NOCTIUM = ITEMS.register("noctium",()->new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+}
